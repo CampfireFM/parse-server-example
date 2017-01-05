389716a547239a5517f07c1e08f8035c0f203bc5
Eric Webb <ericwebb85@yahoo.com>
2017-01-04 19:27:34 -0600

    server index and config changes

--- index.js
+++ index.js
@@ -3,7 +3,10 @@
 
 var express = require('express');
 var ParseServer = require('parse-server').ParseServer;
+var ParseDashboard = require('parse-dashboard');
 var path = require('path');
+
+var allowInsecureHTTP = true
 
 var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
 
@@ -23,6 +26,13 @@ var api = new ParseServer({
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
   }
 });
+
+
+var dashboard = new ParseDashboard({
+                                   // Parse Dashboard settings
+                                   }, allowInsecureHTTP);
+
+
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
 // javascriptKey, restAPIKey, dotNetKey, clientKey
@@ -35,6 +45,12 @@ app.use('/public', express.static(path.join(__dirname, '/public')));
 // Serve the Parse API on the /parse URL prefix
 var mountPath = process.env.PARSE_MOUNT || '/parse';
 app.use(mountPath, api);
+
+// make the Parse Dashboard available at /dashboard
+app.use('/dashboard', dashboard);
+
+//var httpServer = require('http').createServer(app);
+//httpServer.listen(4040);
 
 // Parse Server plays nicely with the rest of your web routes
 app.get('/', function(req, res) {
