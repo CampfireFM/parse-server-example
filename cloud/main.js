efbac15551bf5f37f07a8a2a33fff92eec2cc7d3
Eric Webb <ericwebb85@yahoo.com>
2017-01-08 17:01:27 -0600

    Added after question save method for incrementing users new question count bug fixes

--- cloud/main.js
+++ cloud/main.js
@@ -35,7 +35,7 @@ Parse.Cloud.afterSave("Question", function(request) {
                           var toUser = request.object.get("userRef");
                       
                       
-                      var query = new Parse.Query("_User");
+                      var query = new Parse.Query("User");
 //                      query.get({ useMasterKey: true}).then(......)
                       query.get(toUser["objectId"],{
                                 success: function(user) {
