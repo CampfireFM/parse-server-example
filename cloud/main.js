3c8b29a49393d7460691b1532f2200f813c5946f
Eric Webb <ericwebb85@yahoo.com>
2017-01-19 14:10:53 -0600

    bug fixing like afterSave

--- cloud/main.js
+++ cloud/main.js
@@ -95,135 +95,135 @@ Parse.Cloud.afterSave("Answer", function(request) {
 Parse.cloud.afterSave("Like", function(request) {
     if (request.object.existed() == false) {
 
-        var currentUser = request.user;
-
-        // It's a new "Like"
-        var campfireRef = request.object.get("campfireRef");
-        campfireRef.fetch({
-            success: function(campfire) {
-
-                var questionRef = campfire.object.get("questionRef");
-                questionRef.fetch({
-                    success: function(question) {
-
-                        var questionAsker = question.get("fromUser");
-                        questionAsker.fetch({
-                            success: function(questionAsker) {
-
-                                // Create and save a new "Like" activity for the question Asker
-                                var Activity = Parse.Object.extend("Activity");
-                                var newActivity = new Activity();
-                                newActivity.set("question", question);
-                                newActivity.set("campfire", campfire);
-                                newActivity.set("isRead", false);
-                                newActivity.set("toUser", questionAsker);
-                                newActivity1.set("fromUser", request.user);
-                                newActivity.set("type", "likeToAsker");
-                                newActivity.save(null, { useMasterKey: true });
-
-
-                                // setup a push to the question Answerer
-                                var pushQuery = new Parse.Query(Parse.Installation);
-                                pushQuery.equalTo('deviceType', 'ios');
-                                pushQuery.equalTo('user', questionAsker);
-
-                                var alert = "";
-                                var firstName = currentUser.get('firstName');
-                                var lastName = currentUser.get('lastName');
-                                if (firstName) {
-                                    alert = firstName + " " + lastName + " just liked the answer to your question!";
-                                }
-
-                                Parse.Push.send({
-                                    where: pushQuery,
-                                    data: {
-                                        alert: alert,
-                                        questionId: question.object.id
-                                    }
-                                }, {
-                                    useMasterKey: true,
-                                    success: function() {
-                                        // Push was successful
-                                    },
-                                    error: function(error) {
-                                        throw "PUSH: Got an error " + error.code + " : " + error.message;
-                                    }
-                                });
-
-                            },
-                            useMasterKey: true,
-                            error: function(object, error) {
-                            console.log(error);
-                            throw "Got an error " + error.code + " : " + error.message;
-                            }
-                        });
-
-                        var questionAnswerer = question.get("toUser");
-                        questionAnswerer.fetch({
-                           success: function(toUser) {
-
-                            // Create and save a new "Like" activity
-                            var Activity = Parse.Object.extend("Activity");
-                            var newActivity = new Activity();
-                            newActivity.set("question", question);
-                            newActivity.set("campfire", campfire);
-                            newActivity.set("isRead", false);
-                            newActivity.set("toUser", toUser);
-                            newActivity1.set("fromUser", request.user);
-                            newActivity.set("type", "likeToAnswerer");
-                            newActivity.save(null, { useMasterKey: true });
-
-
-                            // setup a push to the question Asker
-                            var pushQuery = new Parse.Query(Parse.Installation);
-                            pushQuery.equalTo('deviceType', 'ios');
-                            pushQuery.equalTo('user', toUser);
-
-                            var alert = "";
-                            var firstName = currentUser.get('firstName');
-                            var lastName = currentUser.get('lastName');
-                            if (firstName) {
-                                alert = firstName + " " + lastName + " just liked your answer!";
-                            }
-
-                            Parse.Push.send({
-                                where: pushQuery,
-                                data: {
-                                    alert: alert,
-                                    questionId: question.object.id
-                                }
-                            }, {
-                                useMasterKey: true,
-                                success: function() {
-                                    // Push was successful
-                                },
-                                error: function(error) {
-                                    throw "PUSH: Got an error " + error.code + " : " + error.message;
-                                }
-                            });
-                        },
-                        useMasterKey: true,
-                        error: function(object, error) {
-                            console.log(error);
-                            throw "Got an error " + error.code + " : " + error.message;
-                          }
-                       });
-                },
-                useMasterKey: true,
-                error: function(object, error) {
-                    console.log(error);
-                    throw "Got an error " + error.code + " : " + error.message;
-                }
-            });
-            },
-            useMasterKey: true,
-            error: function(object, error) {
-                console.log(error);
-                throw "Got an error " + error.code + " : " + error.message;
-            }
-        });
+    //     var currentUser = request.user;
+    //
+    //     // It's a new "Like"
+    //     var campfireRef = request.object.get("campfireRef");
+    //     campfireRef.fetch({
+    //         success: function(campfire) {
+    //
+    //             var questionRef = campfire.object.get("questionRef");
+    //             questionRef.fetch({
+    //                 success: function(question) {
+    //
+    //                     var questionAsker = question.get("fromUser");
+    //                     questionAsker.fetch({
+    //                         success: function(questionAsker) {
+    //
+    //                             // Create and save a new "Like" activity for the question Asker
+    //                             var Activity = Parse.Object.extend("Activity");
+    //                             var newActivity = new Activity();
+    //                             newActivity.set("question", question);
+    //                             newActivity.set("campfire", campfire);
+    //                             newActivity.set("isRead", false);
+    //                             newActivity.set("toUser", questionAsker);
+    //                             newActivity1.set("fromUser", request.user);
+    //                             newActivity.set("type", "likeToAsker");
+    //                             newActivity.save(null, { useMasterKey: true });
+    //
+    //
+    //                             // setup a push to the question Answerer
+    //                             var pushQuery = new Parse.Query(Parse.Installation);
+    //                             pushQuery.equalTo('deviceType', 'ios');
+    //                             pushQuery.equalTo('user', questionAsker);
+    //
+    //                             var alert = "";
+    //                             var firstName = currentUser.get('firstName');
+    //                             var lastName = currentUser.get('lastName');
+    //                             if (firstName) {
+    //                                 alert = firstName + " " + lastName + " just liked the answer to your question!";
+    //                             }
+    //
+    //                             Parse.Push.send({
+    //                                 where: pushQuery,
+    //                                 data: {
+    //                                     alert: alert,
+    //                                     questionId: question.object.id
+    //                                 }
+    //                             }, {
+    //                                 useMasterKey: true,
+    //                                 success: function() {
+    //                                     // Push was successful
+    //                                 },
+    //                                 error: function(error) {
+    //                                     throw "PUSH: Got an error " + error.code + " : " + error.message;
+    //                                 }
+    //                             });
+    //
+    //                         },
+    //                         useMasterKey: true,
+    //                         error: function(object, error) {
+    //                         console.log(error);
+    //                         throw "Got an error " + error.code + " : " + error.message;
+    //                         }
+    //                     });
+    //
+    //                     var questionAnswerer = question.get("toUser");
+    //                     questionAnswerer.fetch({
+    //                        success: function(toUser) {
+    //
+    //                         // Create and save a new "Like" activity
+    //                         var Activity = Parse.Object.extend("Activity");
+    //                         var newActivity = new Activity();
+    //                         newActivity.set("question", question);
+    //                         newActivity.set("campfire", campfire);
+    //                         newActivity.set("isRead", false);
+    //                         newActivity.set("toUser", toUser);
+    //                         newActivity1.set("fromUser", request.user);
+    //                         newActivity.set("type", "likeToAnswerer");
+    //                         newActivity.save(null, { useMasterKey: true });
+    //
+    //
+    //                         // setup a push to the question Asker
+    //                         var pushQuery = new Parse.Query(Parse.Installation);
+    //                         pushQuery.equalTo('deviceType', 'ios');
+    //                         pushQuery.equalTo('user', toUser);
+    //
+    //                         var alert = "";
+    //                         var firstName = currentUser.get('firstName');
+    //                         var lastName = currentUser.get('lastName');
+    //                         if (firstName) {
+    //                             alert = firstName + " " + lastName + " just liked your answer!";
+    //                         }
+    //
+    //                         Parse.Push.send({
+    //                             where: pushQuery,
+    //                             data: {
+    //                                 alert: alert,
+    //                                 questionId: question.object.id
+    //                             }
+    //                         }, {
+    //                             useMasterKey: true,
+    //                             success: function() {
+    //                                 // Push was successful
+    //                             },
+    //                             error: function(error) {
+    //                                 throw "PUSH: Got an error " + error.code + " : " + error.message;
+    //                             }
+    //                         });
+    //                     },
+    //                     useMasterKey: true,
+    //                     error: function(object, error) {
+    //                         console.log(error);
+    //                         throw "Got an error " + error.code + " : " + error.message;
+    //                       }
+    //                    });
+    //             },
+    //             useMasterKey: true,
+    //             error: function(object, error) {
+    //                 console.log(error);
+    //                 throw "Got an error " + error.code + " : " + error.message;
+    //             }
+    //         });
+    //         },
+    //         useMasterKey: true,
+    //         error: function(object, error) {
+    //             console.log(error);
+    //             throw "Got an error " + error.code + " : " + error.message;
+    //         }
+    //     });
     }
-})
+});
 
 
 
