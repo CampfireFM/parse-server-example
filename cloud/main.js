f1469141fdb8d3afce51ff641230ff35a91b34fd
Eric Webb <ericwebb85@yahoo.com>
2017-04-07 14:40:01 -0500

    cleanup

--- cloud/main.js
+++ cloud/main.js
@@ -197,20 +197,9 @@ Parse.Cloud.define('getFeaturedTopics', function(req, res) {
   // var topics = [];
   var List = Parse.Object.extend('List');
   var query = new Parse.Query(List);
-  query.equalTo("objectId",'CTsXJi51Qc');
+  query.equalTo("objectId",'06LOiNkyrJ');
   query.find({   
     success: function(topics) {
-      // if (objects.length) {
-      //   for (var i = 0; i < objects.length; i++) {
-      //   var object = objects[i];
-      //     topics.push({
-      //       id: object.objectId,
-      //       name: object.get('name'),
-      //       type: object.get('type'),
-    //       image: object.get('image') ? (object.get('image')).toJSON().url : ''
-      //     });
-      //   }
-      // }
       res.success(topics);
     },
     error: function(error) {
