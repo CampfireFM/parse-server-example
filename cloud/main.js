4e8ea7bdbe7bca01706a0b986076414810dd2b19
Eric Webb <ericwebb85@yahoo.com>
2017-04-07 14:35:08 -0500

    cleanup

--- cloud/main.js
+++ cloud/main.js
@@ -197,9 +197,9 @@ Parse.Cloud.define('getFeaturedTopics', function(req, res) {
   // var topics = [];
   var List = Parse.Object.extend('List');
   var query = new Parse.Query(List);
-  query.query.equalTo("objectId",'CTsXJi51Qc');
-  query.find({
-    success: function(objects) {
+  query.equalTo("objectId",'CTsXJi51Qc');
+  query.find({   
+    success: function(topics) {
       // if (objects.length) {
       //   for (var i = 0; i < objects.length; i++) {
       //   var object = objects[i];
@@ -207,11 +207,11 @@ Parse.Cloud.define('getFeaturedTopics', function(req, res) {
       //       id: object.objectId,
       //       name: object.get('name'),
       //       type: object.get('type'),
-      //       image: object.get('image') ? (object.get('image')).toJSON().url : ''
+    //       image: object.get('image') ? (object.get('image')).toJSON().url : ''
       //     });
       //   }
       // }
-      res.success(objects);
+      res.success(topics);
     },
     error: function(error) {
       response.error(error);
