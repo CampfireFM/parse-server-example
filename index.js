f07aa2d8a09761037f96ca4c9b723859dd5197fb
hbkrunal <hb.krunalrana@hiddenbrains.in>
2017-04-11 15:23:28 +0530

    Made default image URI to http

--- index.js
+++ index.js
@@ -256,7 +256,7 @@ app.get('/meta/*', function(req, res) {
           error: function(object, error) {
             return res.render('eavesdrop_meta',{
               page: req.params[0],
-              imageUrl: 'https://campfire.fm/images/siteLogo.png',
+              imageUrl: 'http://campfire.fm/images/siteLogo.png',
               fb_app_id: config.facebookAppIds[0],
               description: "Spark intimate conversations that reward you, your heroes, and the causes you care about.",
               title: "Campfire - Hear it here."
@@ -267,7 +267,7 @@ app.get('/meta/*', function(req, res) {
     else{
       return res.render('eavesdrop_meta',{
         page: page,
-        imageUrl: 'https://campfire.fm/images/logo.png',
+        imageUrl: 'http://campfire.fm/images/siteLogo.png',
         fb_app_id: config.facebookAppIds[0],
         description: "Spark intimate conversations that reward you, your heroes, and the causes you care about.",
         title: "Campfire - Hear it here."
