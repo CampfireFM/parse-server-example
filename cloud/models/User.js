2c7f29b629cf4107fb8cd2a6f648d68b2e460d8d
Eric Webb <ericwebb85@yahoo.com>
2017-04-14 17:56:42 -0500

    Added logging to before save

--- cloud/models/User.js
+++ cloud/models/User.js
@@ -21,10 +21,13 @@ Parse.Cloud.afterSave(Parse.User, function(request, response) {
     response.success('ok');
 });
 
+
 Parse.Cloud.beforeSave(Parse.User, function(request, response) {
 
     const userEmail = request.object.get('email');
     isUpdating = userEmail !== undefined;
+    console.log(userEmail);
+    console.log(isUpdating);
     if(isUpdating == true){
         var query = new Parse.Query(Parse.User);
         query.get(request.object.id, {useMasterKey : true}).then(function(result){
