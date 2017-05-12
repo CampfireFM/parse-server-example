665ca3112158160f77e2c1a675435608d51b04f4
Shanmuk Yerramsetty <hb.shanmuk@hiddenbrains.in>
2017-05-12 21:11:22 +0530

    Implement webview sessions

--- cloud/models/User.js
+++ cloud/models/User.js
@@ -4,6 +4,7 @@ const {sendPush} = require('../common');
 var Twitter = require('twitter');
 var Mixpanel = require('mixpanel');
 var graph = require('fbgraph');
+var uniqid = require('uniqid');
 var oldEmail = '';
 Parse.Cloud.afterSave(Parse.User, function(request, response) {
     const userEmail = request.object.get('email');
@@ -122,6 +123,58 @@ Parse.Cloud.afterSave(Parse.User, function(request, response) {
     }
     response.success('ok');
 });
+
+Parse.Cloud.define('validateWvSession', function(req, res){
+    var validUser = Parse.Object.extend('User');
+    var validUserQuery = new Parse.Query(validUser);
+    validUserQuery.equalTo("wvSessionToken", req.params.wvSessionToken)
+    var password = uniqid(); //create new random passowrd
+    validUserQuery.first({useMasterKey : true}).then(function(validUserObj) {
+        var userObjId = validUserObj.id;
+        var newToken = uniqid(userObjId+'-');
+        validUserObj.set("password", password);
+        validUserObj.set('wvSessionToken', newToken);
+        validUserObj.save(null, {useMasterKey : true}).then(function(newUserObj){
+            Parse.User.logIn(newUserObj.get("username"), password).then(function(user){
+                var session_token = user.getSessionToken()
+                res.success({session_token: session_token});
+            }, function(error){
+                console.log('Got an error ' + error.code + ' : ' + error.message);
+                res.error(error);
+            })
+        }, function(error){
+            console.log('Got an error ' + error.code + ' : ' + error.message);
+            res.error(error);
+        });
+    }, function(error) {
+        console.log('Got an error ' + error.code + ' : ' + error.message);
+        res.error(error);
+    });
+});
+
+Parse.Cloud.define('getWebViewToken', function(req, res){
+    var wvUser = Parse.Object.extend('User');
+    var wvUserQuery = new Parse.Query(wvUser);
+    wvUserQuery.get(req.userId, {
+      success: function(user) {
+        var userObjId = user.id;
+        var newToken = uniqid(userObjId+'-');
+        user.set('wvSessionToken', newToken);
+        user.save(null, {useMasterKey : true}).then(function(newUserObj){
+            res.success({webViewToken: newUserObj.get("wvSessionToken")});
+        }, function(error){
+            console.log('Got an error ' + error.code + ' : ' + error.message);
+            res.error(error);
+        });
+      },
+      error: function(error) {
+        console.log('Got an error ' + error.code + ' : ' + error.message);
+        res.error(error);
+      }
+    });
+});
+
+
 
 function getUsersByFacebookIds(facebookIds, callback){
     var query = new Parse.Query(Parse.User);
