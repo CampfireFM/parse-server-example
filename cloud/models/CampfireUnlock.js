ea5759397ce09995fee482f94932e6ef719afee3
prosunshining <prosunshining@gmail.com>
2017-05-30 19:51:44 +0700

    don't add myself to activity when I eavesdrop my answer/question

--- cloud/models/CampfireUnlock.js
+++ cloud/models/CampfireUnlock.js
@@ -11,26 +11,30 @@ Parse.Cloud.afterSave("CampfireUnlock", function(request) {
         answerRef.fetch({
             success: function (answer) {
                 var questionRef = answer.get("questionRef");
-                getQuestionObjAndItsPointers(questionRef.id, function(err_question, complete_question){
-                        if(err_question){
-                            request.log.error(err_question);
-                            console.log(err_question);
-                        }else{
-                            var params = {
-                                question: complete_question,
-                                campfireunlock: request.object,
-                                answer : answer
-                            };
-                            answer.increment("unlockCount",1);
-                            answer.save();
+                getQuestionObjAndItsPointers(questionRef.id, function(err_question, complete_question) {
+                    if (err_question) {
+                        request.log.error(err_question);
+                        console.log(err_question);
+                    } else {
+                        var params = {
+                            question: complete_question,
+                            campfireunlock: request.object,
+                            answer: answer
+                        };
+                        answer.increment("unlockCount", 1);
+                        answer.save();
 
-                            splitUnlockEarnings(params);
-                            const toUsers = [complete_question.get('fromUser'), complete_question.get('toUser')];
-                            //Create 'unlock' activity
-                            addActivity('unlock', currentUser, toUsers, complete_question, answer);
-                            //Send push notification to question asker and answerer
-                            sendPushOrSMS(currentUser, toUsers, 'unlocks');
-                        }
+                        splitUnlockEarnings(params);
+                        var toUsers = [];
+                        if(complete_question.get('fromUser').id !== request.user.id)
+                            toUsers.push(complete_question.get('fromUser'));
+                        if(complete_question.get('toUser').id != request.user.id)
+                            toUsers.push(complete_question.get('toUser'));
+                        //Create 'unlock' activity
+                        addActivity('unlock', currentUser, toUsers, complete_question, answer);
+                        //Send push notification to question asker and answerer
+                        sendPushOrSMS(currentUser, toUsers, 'unlocks');
+                    }
                 });
             },
             useMasterKey: true,
@@ -48,163 +52,161 @@ and their charity
 */
 function splitUnlockEarnings(params){
 
-      console.log("reached here11");
-      var question = params.question;
-      var total_unlock_earnings = campfireUnlockValue;
+    console.log("reached here11");
+    var question = params.question;
+    var total_unlock_earnings = campfireUnlockValue;
     
-      var fromUser = question.get("fromUser");
-      var toUser   = question.get("toUser");
+    var fromUser = question.get("fromUser");
+    var toUser   = question.get("toUser");
 
-      var split_asker = total_unlock_earnings / 2;
-      var split_answerer = total_unlock_earnings / 2;
+    var split_asker = total_unlock_earnings / 2;
+    var split_answerer = total_unlock_earnings / 2;
 
-      var answerer_charity_percentage = question.get("charityPercentage") ? question.get("charityPercentage") : 0;
-      var split_answerer_charity = split_answerer * ( answerer_charity_percentage / 100);
+    var answerer_charity_percentage = question.get("charityPercentage") ? question.get("charityPercentage") : 0;
+    var split_answerer_charity = split_answerer * ( answerer_charity_percentage / 100);
 
-      var asker_charity_percentage = fromUser.get("donationPercentage") ? fromUser.get("donationPercentage") : 0;
-      var split_asker_charity = split_asker * ( asker_charity_percentage / 100);
+    var asker_charity_percentage = fromUser.get("donationPercentage") ? fromUser.get("donationPercentage") : 0;
+    var split_asker_charity = split_asker * ( asker_charity_percentage / 100);
 
-      var asker_charity = fromUser.get("charityRef");
+    var asker_charity = fromUser.get("charityRef");
 
-      var split_asker_final = split_asker - split_asker_charity;
-      var split_answerer_final = split_answerer - split_answerer_charity;
+    var split_asker_final = split_asker - split_asker_charity;
+    var split_answerer_final = split_answerer - split_answerer_charity;
 
-      var payout_asker_params = {
-            amount : split_asker_final,
-            userRef : fromUser,
-            unlockRef : params.campfireunlock,
-            type : 'unlockAsker',
-            isPaid : false
-      };
+    var payout_asker_params = {
+        amount: split_asker_final,
+        userRef: fromUser,
+        unlockRef: params.campfireunlock,
+        type: 'unlockAsker',
+        isPaid: false
+    };
 
-      console.log(payout_asker_params);
+    console.log(payout_asker_params);
 
-        createPayoutForUnlock(payout_asker_params, function(e,r){
-            console.log(e);
-            console.log();
-        });
+    createPayoutForUnlock(payout_asker_params, function(e,r) {
+        console.log(e);
+    });
 
-      var payout_answerer_params = {
-            amount : split_answerer_final,
-            userRef : toUser,
-            unlockRef : params.campfireunlock,
-            type : 'unlockAnswerer',
-            isPaid : false
-      };
+    var payout_answerer_params = {
+        amount: split_answerer_final,
+        userRef: toUser,
+        unlockRef: params.campfireunlock,
+        type: 'unlockAnswerer',
+        isPaid: false
+    };
 
-        createPayoutForUnlock(payout_answerer_params, function(e,r){
-            console.log(e);
-            console.log();
-        });
+    createPayoutForUnlock(payout_answerer_params, function(e,r) {
+        console.log(e);
+        console.log();
+    });
 
-      var donation_answerer_params = {
-            amount: split_answerer_charity,
-            charityRef: question.get("charity"),
-            questionRef: question,
-            userRef : toUser,
-            isPaid: false
-      };
+    var donation_answerer_params = {
+        amount: split_answerer_charity,
+        charityRef: question.get("charity"),
+        questionRef: question,
+        userRef: toUser,
+        isPaid: false
+    };
+    createDonationForUnlock(donation_answerer_params, function (e, r) {
+        console.log(e);
+        console.log();
+    });
 
-      createDonationForUnlock(donation_answerer_params, function(e,r){
-            console.log(e);
-            console.log();
-        });
+    var donation_asker_params = {
+        amount: split_asker_charity,
+        charityRef: asker_charity,
+        questionRef: question,
+        userRef: fromUser,
+        isPaid: false
+    };
 
-      var donation_asker_params = {
-            amount: split_asker_charity,
-            charityRef: asker_charity,
-            questionRef: question,
-            userRef : fromUser,
-            isPaid: false
-      };
+    createDonationForUnlock(donation_asker_params, function (e, r) {
+        console.log(e);
+        console.log();
+    });
 
-      createDonationForUnlock(donation_asker_params, function(e,r){
-            console.log(e);
-            console.log();
-        });
 
-    
-      // The following should probably go inside a payout or donation success block
-    
-      fromUser.increment("earningsTotal", split_asker);
-      fromUser.increment("earningsBalance", split_asker);
-      fromUser.increment("earningsDonated", donation_asker_params["amount"]);
-      fromUser.save(null, {useMasterKey: true});
+    // The following should probably go inside a payout or donation success block
 
-      toUser.increment("earningsTotal", split_answerer);
-      toUser.increment("earningsBalance", split_answerer);
-      toUser.increment("earningsDonated", donation_answerer_params["amount"]);
-      toUser.save(null, {useMasterKey: true});
+    fromUser.increment("earningsTotal", split_asker);
+    fromUser.increment("earningsBalance", split_asker);
+    fromUser.increment("earningsDonated", donation_asker_params["amount"]);
+    fromUser.save(null, {useMasterKey: true});
+
+    toUser.increment("earningsTotal", split_answerer);
+    toUser.increment("earningsBalance", split_answerer);
+    toUser.increment("earningsDonated", donation_answerer_params["amount"]);
+    toUser.save(null, {useMasterKey: true});
 }
 
 /*
 @Description : Function to create Charity record
 */
-function createDonationForUnlock(params, callback){
+function createDonationForUnlock(params, callback) {
 
-      var Donation = Parse.Object.extend("Donation");
-      var donation = new Donation();
+    var Donation = Parse.Object.extend("Donation");
+    var donation = new Donation();
 
-      for(key in params){
-            donation.set(key,params[key]);
-      }
+    for (key in params) {
+        donation.set(key, params[key]);
+    }
 
-      donation.save(null, {
-            useMasterKey: true,
-            success: function(donationrecord){
-                return callback(null,donationrecord);
-            },error : function(err){
-                return callback(err,null);
-            }
-      });
-      //end of save operation code block
+    donation.save(null, {
+        useMasterKey: true,
+        success: function (donationrecord) {
+            return callback(null, donationrecord);
+        }, error: function (err) {
+            return callback(err, null);
+        }
+    });
+    //end of save operation code block
 }
 
 /*
 @Description : Function to create Payout record
 */
-function createPayoutForUnlock(params, callback){
+function createPayoutForUnlock(params, callback) {
 
-      var Payout = Parse.Object.extend("Payout");
-      var payout = new Payout();
+    var Payout = Parse.Object.extend("Payout");
+    var payout = new Payout();
 
-      for(key in params){
-            payout.set(key,params[key]);
-      }
+    for (key in params) {
+        payout.set(key, params[key]);
+    }
 
-      payout.save(null, {
-            useMasterKey: true,
-            success: function(payoutrecord){
-                return callback(null,payoutrecord);
-            },error : function(err){
-                return callback(err,null);
-            }
-      });
-      //end of save operation code block
+    payout.save(null, {
+        useMasterKey: true,
+        success: function (payoutrecord) {
+            return callback(null, payoutrecord);
+        }, error: function (err) {
+            return callback(err, null);
+        }
+    });
+    //end of save operation code block
 }
 
 
 
-function getQuestionObjAndItsPointers(questionId,callback){
+function getQuestionObjAndItsPointers(questionId,callback) {
 
-      console.log("inside getQuestionObjAndItsPointers");
-      var Question = Parse.Object.extend("Question");
-      var query = new Parse.Query(Question);
-      query.include(['fromUser','fromUser.charityRef','toUser','toUser.charityRef']);
-      query.equalTo("objectId",questionId);
-      query.find({
-        success: function(questions) {
-          console.log("in the SUCCESS of getQuestionObjAndItsPointers");
-          console.log(questions.length);
-          console.log(questions[0]);
-          // return res.success(questions[0]);
-          return callback(null,questions[0]);
+    console.log("inside getQuestionObjAndItsPointers");
+    var Question = Parse.Object.extend("Question");
+    var query = new Parse.Query(Question);
+    query.include(['fromUser', 'fromUser.charityRef', 'toUser', 'toUser.charityRef']);
+    query.equalTo("objectId", questionId);
+    query.find({
+        success: function (questions) {
+            console.log("in the SUCCESS of getQuestionObjAndItsPointers");
+            console.log(questions.length);
+            console.log(questions[0]);
+            // return res.success(questions[0]);
+            return callback(null, questions[0]);
         },
-        error: function(object, error) {
-          console.log("in the FAILURE of getQuestionObjAndItsPointers");
-          console.log(error);
-          return callback(error,null);
-          // return res.error(error);
+        error: function (object, error) {
+            console.log("in the FAILURE of getQuestionObjAndItsPointers");
+            console.log(error);
+            return callback(error, null);
+            // return res.error(error);
         }
-      });
+    });
 }
