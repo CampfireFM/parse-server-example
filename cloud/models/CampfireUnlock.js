55e6c910fabffca105f1f7f66cf24c2b7532d81c
prosunshining <prosunshining@gmail.com>
2017-06-26 20:55:57 +0400

    fixed attribute name mismatch issue for earningFromUnlocks

--- cloud/models/CampfireUnlock.js
+++ cloud/models/CampfireUnlock.js
@@ -101,7 +101,7 @@ function splitUnlockEarnings(params){
 
         fromUser.increment("earningsTotal", split_asker);
         fromUser.increment("earningsBalance", split_asker);
-        fromUser.increment("earningsFromUnlock", split_asker);
+        fromUser.increment("earningsFromUnlocks", split_asker);
         fromUser.increment("earningsDonated", split_asker_charity);
         fromUser.save(null, {useMasterKey: true});
     }
@@ -138,7 +138,7 @@ function splitUnlockEarnings(params){
 
     toUser.increment("earningsTotal", split_answerer);
     toUser.increment("earningsBalance", split_answerer);
-    toUser.increment("earningsFromUnlock", split_answerer);
+    toUser.increment("earningsFromUnlocks", split_answerer);
     toUser.increment("earningsDonated", split_answerer_charity);
     toUser.save(null, {useMasterKey: true});
 }
