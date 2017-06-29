79343a4f05145d621cb2f8efe23a5d9b344f34e6
Shanmuk Yerramsetty <hb.shanmuk@hiddenbrains.in>
2017-06-29 20:30:48 +0530

    implement spotlighted lists

--- cloud/topic.js
+++ cloud/topic.js
@@ -34,11 +34,65 @@ Parse.Cloud.define('getFeaturedTopics', function(req, res) {
     var query = new Parse.Query(List);
     query.containedIn('objectId', spotlightedLists);
     query.find({useMasterKey: true}).then(function(topics){
-        res.success(topics);
+        res.success({topics: topics, spotlightedLists: spotlightedLists.reverse() });
       }, function(error) {
         res.error(error);
       }
     );
+  }, function(error){
+    console.log(error);
+    res.error(error);
+  })
+});
+
+Parse.Cloud.define('setFeaturedTopics', function(req, res) {
+  var Defaults = Parse.Object.extend('Defaults');
+  var defaultQuery = new Parse.Query(Defaults);
+  defaultQuery.first({useMasterKey: true}).then(function(defaultValue){
+    var spotlightedLists = defaultValue.get('spotlightedLists');
+    if (req.params.featuredLists) {
+      defaultValue.remove('spotlightedLists');
+      defaultValue.set('spotlightedLists', req.params.featuredLists);
+    } else {
+      if (spotlightedLists.indexOf(req.params.listId) == -1 ) {
+        if (spotlightedLists.length > 2) {
+          spotlightedLists.pop()
+        }
+        spotlightedLists.push(req.params.listId)
+        defaultValue.set('spotlightedLists', spotlightedLists);
+      }
+    }
+    defaultValue.save(null, {
+      success: function(defaultObj) {
+        res.success(defaultObj);
+      },
+      error: function(defaultObj, error) {
+        res.error(error);
+      }
+    });
+  }, function(error){
+    console.log(error);
+    res.error(error);
+  })
+});
+
+Parse.Cloud.define('RemoveFeaturedTopic', function(req, res) {
+  var Defaults = Parse.Object.extend('Defaults');
+  var defaultQuery = new Parse.Query(Defaults);
+  defaultQuery.first({useMasterKey: true}).then(function(defaultValue){
+    var spotlightedLists = defaultValue.get('spotlightedLists');
+    var listIdIndex = spotlightedLists.indexOf(req.params.listId);
+    spotlightedLists.splice(listIdIndex, 1);
+    defaultValue.remove('spotlightedLists');
+    defaultValue.set('spotlightedLists', spotlightedLists);
+    defaultValue.save(null, {
+      success: function(defaultObj) {
+        res.success(defaultObj);
+      },
+      error: function(defaultObj, error) {
+        res.error(error);
+      }
+    });
   }, function(error){
     console.log(error);
     res.error(error);
