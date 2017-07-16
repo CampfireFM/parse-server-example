0cc81d992e7685c69f216f9a666bfc3253e40968
prosunshining <prosunshining@gmail.com>
2017-07-16 07:59:19 +0700

    fix small issue

--- cloud/models/CampfireUnlock.js
+++ cloud/models/CampfireUnlock.js
@@ -154,7 +154,7 @@ function createDonationForUnlock(params, callback) {
     var Donation = Parse.Object.extend("Donation");
     var donation = new Donation();
 
-    if(params.amount === 0 || !params.charityRef)
+    if(params.amount === 0)
         return callback(null);
     for (key in params) {
         donation.set(key, params[key]);
