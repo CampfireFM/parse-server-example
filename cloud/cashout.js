5eca64c80b8760f273c77938c143d09267fde9fd
Luke Vink <luke@lukevink.com>
2017-10-27 10:57:22 +0100

    Changed in parse to “cashOutAmount”

--- cloud/cashout.js
+++ cloud/cashout.js
@@ -30,7 +30,7 @@ Parse.Cloud.define('getCashouts', function(req, res){
             id: object.id,
             status: object.get('status'),
             userFullName: object.get('userRef').get('fullName'),
-            cashOutAmount: object.get('earningsBalance'),
+            cashOutAmount: object.get('cashOutAmount'),
             userId: object.get('userRef').id,
             paypalEmail: object.get('paypalEmail')
           });
