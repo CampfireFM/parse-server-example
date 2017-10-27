ecb5578239009d3f24a6cba629c47af5f7c3cf3f
Luke Vink <luke@lukevink.com>
2017-10-27 11:12:25 +0100

    Added Cashout Amount to Request Cashout Params

--- cloud/main.js
+++ cloud/main.js
@@ -1166,10 +1166,12 @@ Parse.Cloud.define('getFriendsMatch', function(request, response){
 Parse.Cloud.define('requestCashout', function(request, response){
     var currentUser = request.user;
     var paypalEmail = request.params.paypalEmail;
-
+    var cashOutAmount = request.params.cashOutAmount;
+    
     const cashout = new Parse.Object('Cashout');
     cashout.set('userRef', currentUser);
     cashout.set('paypalEmail', paypalEmail);
+    cashout.set('cashOutAmount', cashOutAmount);
     cashout.set('isConfirmed', false);
     cashout.set('isPaid', false);
     cashout.set('status', 'Pending');
